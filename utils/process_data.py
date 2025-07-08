fc0e8254ddf989e16b2d2152cd87732f19ed0b15
tk0910 <141562923+tkhangg0910@users.noreply.github.com>
2025-07-08 16:56:23 +0700

    Update process_data.py

--- utils/process_data.py
+++ utils/process_data.py
@@ -130,23 +130,23 @@ def save_synset_ids(synset_ids, path: str):
     print(f"💾 Saved synset_id mapping to: {path}")
 
 
-if __name__ == "__main__":
-    split_contrastive_stage1_data(
-        "data/raw/stage_1_pseudo_sents/pseudo_sents_v1.json",
-        "data/raw/stage_1_pseudo_sents/word_synsets_with_pos_with_gloss_v2.csv",
-        "data/processed/stage1_pseudo_sents"
-    )
-
 # if __name__ == "__main__":
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument("--gloss_path", type=str, required=True, help="Path to gloss_embeddings.pt")
-#     parser.add_argument("--index_out", type=str, default="gloss_faiss.index", help="FAISS index output file")
-#     parser.add_argument("--id_out", type=str, default="synset_ids.pt", help="Synset ID output file")
-#     parser.add_argument("--use_gpu", action="store_true", help="Use GPU for indexing")
+#     split_contrastive_stage1_data(
+#         "data/raw/stage_1_pseudo_sents/pseudo_sents_v1.json",
+#         "data/raw/stage_1_pseudo_sents/word_synsets_with_pos_with_gloss_v2.csv",
+#         "data/processed/stage1_pseudo_sents"
+#     )
 
-#     args = parser.parse_args()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--gloss_path", type=str, required=True, help="Path to gloss_embeddings.pt")
+    parser.add_argument("--index_out", type=str, default="gloss_faiss.index", help="FAISS index output file")
+    parser.add_argument("--id_out", type=str, default="synset_ids.pt", help="Synset ID output file")
+    parser.add_argument("--use_gpu", action="store_true", help="Use GPU for indexing")
 
-#     embd, synset_ids = load_gloss_embeddings(args.gloss_path)
-#     index = index_embeddings(embd, use_gpu=args.use_gpu)
-#     save_index(index, args.index_out)
-#     save_synset_ids(synset_ids, args.id_out)
+    args = parser.parse_args()
+
+    embd, synset_ids = load_gloss_embeddings(args.gloss_path)
+    index = index_embeddings(embd, use_gpu=args.use_gpu)
+    save_index(index, args.index_out)
+    save_synset_ids(synset_ids, args.id_out)
