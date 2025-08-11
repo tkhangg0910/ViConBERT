52ae8c083e991fe23fc1df556cd16f74b5d22b6a
tk0910 <141562923+tkhangg0910@users.noreply.github.com>
2025-08-11 17:44:13 +0700

    Update utils.py

--- trainings/utils.py
+++ trainings/utils.py
@@ -158,7 +158,7 @@ def train_model(num_epochs, train_data_loader, valid_data_loader,
 
                 train_pbar.set_postfix()
             
-            del outputs, loss, gloss_embd, context_input_ids, context_attention_mask, target_spans, synset_ids
+            # del outputs, loss, gloss_embd, context_input_ids, context_attention_mask, target_spans, synset_ids
             
             # if scheduler:
             #     scheduler.step() 
