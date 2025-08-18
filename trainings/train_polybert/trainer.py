bd85b53a85ef606033936cd5a926352c646e6e4b
tk0910 <141562923+tkhangg0910@users.noreply.github.com>
2025-08-18 17:47:35 +0700

    UPDATE

--- trainings/train_polybert/trainer.py
+++ trainings/train_polybert/trainer.py
@@ -82,7 +82,7 @@ if __name__=="__main__":
             )
 
     else:
-        train_dataset = PolyBERTtDataseV2(train_sample, tokenizer, train_gloss_size= 128 )
+        train_dataset = PolyBERTtDataseV2(train_sample, tokenizer, train_gloss_size= 5 )
         valid_dataset = PolyBERTtDataseV2(valid_sample, tokenizer,val_mode=True)
     
     # sampler = ContrastiveBatchSampler(train_dataset,batch_size=batch_size)
