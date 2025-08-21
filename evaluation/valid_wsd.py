62c40f3c94aa0ee35f00e2b620aa40a0f3e56f2b
tk0910 <141562923+tkhangg0910@users.noreply.github.com>
2025-08-21 22:16:13 +0700

    Update valid_wsd.py

--- evaluation/valid_wsd.py
+++ evaluation/valid_wsd.py
@@ -579,7 +579,7 @@ if __name__ == "__main__":
             print("WSD_BEMDataset")
             valid_set = WSD_BEMDataset(valid_sample, tokenizer, val_mode=True)
         elif args.model_type =="poly":
-            valid_set = WSD_ViConDataset(valid_sample, tokenizer, mode="gloss_model", 
+            valid_set = WSD_ViConDataset(valid_sample, tokenizer, mode="gloss_model"
                                    , val_mode=True)
     
     # Print POS distribution
